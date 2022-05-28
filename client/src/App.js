e3a34ff9b6472907646dd15e5ea9648e232a9a36
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-05-28 15:39:36 +0600

    add fatrash, faedit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,40 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons'
 import Header from './components/Header';
+import AddTodo from './components/AddTodo';
 
 
 const App = () => {
   const [tasks, setTasks] = useState([])
   useEffect(()=>{
     axios('http://localhost:8000/api/tasks')
-      .then(({data})=> setTasks(data))
+      .then(({data})=> {
+        setTasks(data)
+     
+      })
   },[])
-
+const addTask = newTask=>{
+  setTasks([...tasks, newTask])
+}
   return (
     <div className=''>
     <Header/>
+    <AddTodo addTask={addTask}/>
      <ul className='list-group'>
        {
          tasks.map(item=>
            <li className='list-group-item col-md-4 offset-md-4 d-flex justify-content-between align-items-center'>{item.title}
-           <button className='btn btn-primary btn-sm'>Delete</button>
+           <div>
+           <button className='btn btn-primary btn-sm mr-2'>
+             <FontAwesomeIcon icon={faTrash}/>
+           </button>
+           <button className='btn btn-danger btn-sm'>
+             <FontAwesomeIcon icon={faEdit}/>
+           </button>
+           </div>
+       
            </li>
          )
        }
--- client/src/components/AddTodo/index.js
+++ client/src/components/AddTodo/index.js
@@ -1,8 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const AddTodo = () => {
+import axios from 'axios';
+
+const AddTodo = (props) => {
+  const [task, setTask] = useState('')
+  const addTask =()=>{
+    if(task.trim()===''){
+      return
+    }else{
+      axios.post('http://localhost:8000/api/posts' ,
+      {
+        title:task
+      } 
+    ).then(res=>{
+      setTask('')
+      props.addTask(res.data)
+
+    }).catch(err=> console.log(err))
+  }
+
+
+
+
+  }
   return (
-	 <div>AddTodo</div>
+   
+      <div className='flex items-center mt-5 mb-5 max-w-md mx-auto bg-white rounded-lg '>
+     
+      <div className="w-full">
+          <input type="search" class="form-control me-3"
+            placeholder="Add todo" 
+            x-model="search" 
+            value={task}
+            required
+        onChange={event =>setTask(event.target.value)}
+            />
+        </div>
+        <button  
+        className="btn btn-primary"
+    onClick={()=>addTask()}
+        >
+          Add
+        </button>
+     
+       
+      </div>
+
+
+
   )
 }
 
